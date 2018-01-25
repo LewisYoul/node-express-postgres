1be768e5e2160e4e69b1ceff83b1235dc162d687
Lewis <lewisyoul@gmail.com>
2018-01-25 15:35:57 +0000

    Set up basic server that renders a message as JSON

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', function(req, res) {
+  res.status(200).send({ message: 'The app is up!' })
+});
+
+module.exports = app
